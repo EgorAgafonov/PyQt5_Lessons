0aa4dfef9b8aea3554740fdeaa7fdc84c2677894
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-07-05 16:03:00 +0300

    First commit and upload

--- practic.py
+++ practic.py
@@ -13,17 +13,43 @@ class MainWindow(qtw.QMainWindow):
         self.setWindowTitle('Практика')
         self.setGeometry(960, 440, 500, 250)
 
+        # Result field
+        self.result_field = qtw.QLineEdit(self)
+        self.result_field.setReadOnly(True)
+        self.result_field.move(150, 50)
+        self.result_field.setFixedWidth(200)
+
         # Text object
         self.main_text = qtw.QLabel(self)
         self.main_text.setText('Тестовая надпись в основном окне')
         self.main_text.move(150, 20)
         self.main_text.adjustSize()
 
-        # Button object
-        self.btn = qtw.QPushButton(self)
-        self.btn.move(150, 50)
-        self.btn.setText('Укажите логин и пароль \nпользователя')
-        self.btn.setFixedWidth(200)
+        # Result button object
+        self.btn_res = qtw.QPushButton(self)
+        self.btn_res.move(100, 90)
+        self.btn_res.setText('Укажите логин и пароль \nпользователя')
+        self.btn_res.setFixedWidth(200)
+        self.btn_res.clicked.connect(self._show_result)
+
+        # Clear button object
+        self.btn_clr = qtw.QPushButton(self)
+        self.btn_res.move(150, 90)
+        self.btn_res.setText('Очистить поле вывода')
+        self.btn_res.setFixedWidth(200)
+        self.btn_res.clicked.connect(self._show_result)
+
+    def _show_result(self):
+        """"""
+
+        result = "Данные отсутствуют"
+        self.result_field.setText(result)
+
+
+
+
+
+
 
 
 def run_application():
