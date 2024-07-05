1b229e397ebcbc4148b1cd6c3e9b9dcf8ea6ea22
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-07-05 11:41:10 +0300

    First commit and upload

--- practic.py
+++ practic.py
@@ -4,11 +4,28 @@ import tkinter
 
 
 def application():
+    """"""
+
     app = qtw.QApplication(sys.argv)
+    app.setStyle(qtw.QStyleFactory.create("Fusion"))
+
+    # Main window (one)
     mw = qtw.QMainWindow()
+    main_text = qtw.QLabel(mw)
     mw.setWindowTitle('Практика')
-    mw.setGeometry(960, 540, 500, 250)
-    mw.setStyleSheet('Fusion')
+    mw.setGeometry(960, 440, 500, 250)
+
+    # Text object
+    main_text.setText('Тестовая надпись в основном окне')
+    main_text.move(150, 20)
+    main_text.adjustSize()
+
+    # Button object
+    btn = qtw.QPushButton(mw)
+    btn.move(150, 50)
+    btn.setText('Укажите логин и пароль \nпользователя')
+    btn.setFixedWidth(200)
+
     mw.show()
     sys.exit(app.exec_())
 
