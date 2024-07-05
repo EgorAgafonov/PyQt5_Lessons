f564328b4adb0cc99f4a43d0542a9fe34520f788
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-07-05 12:48:43 +0300

    First commit and upload

--- practic.py
+++ practic.py
@@ -3,32 +3,40 @@ import sys
 import tkinter
 
 
-def application():
+class MainWindow(qtw.QMainWindow):
+    """"""
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        # Main window (one)
+        self.setWindowTitle('Практика')
+        self.setGeometry(960, 440, 500, 250)
+
+        # Text object
+        self.main_text = qtw.QLabel(self)
+        self.main_text.setText('Тестовая надпись в основном окне')
+        self.main_text.move(150, 20)
+        self.main_text.adjustSize()
+
+        # Button object
+        self.btn = qtw.QPushButton(self)
+        self.btn.move(150, 50)
+        self.btn.setText('Укажите логин и пароль \nпользователя')
+        self.btn.setFixedWidth(200)
+
+
+def run_application():
     """"""
 
     app = qtw.QApplication(sys.argv)
     app.setStyle(qtw.QStyleFactory.create("Fusion"))
-
-    # Main window (one)
-    mw = qtw.QMainWindow()
-    main_text = qtw.QLabel(mw)
-    mw.setWindowTitle('Практика')
-    mw.setGeometry(960, 440, 500, 250)
-
-    # Text object
-    main_text.setText('Тестовая надпись в основном окне')
-    main_text.move(150, 20)
-    main_text.adjustSize()
-
-    # Button object
-    btn = qtw.QPushButton(mw)
-    btn.move(150, 50)
-    btn.setText('Укажите логин и пароль \nпользователя')
-    btn.setFixedWidth(200)
-
+    mw = MainWindow()
     mw.show()
-    sys.exit(app.exec_())
+    app.exec_()
 
 
-application()
+run_application()
+
+
 
